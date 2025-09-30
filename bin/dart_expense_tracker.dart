77f33948293ab4ddfd3953e945f3480a7ff89615
Nugetzrul3 <popingcorn12@gmail.com>
2025-09-30 20:05:52 +1000

    Added functionality to update and delete tasks

--- bin/dart_expense_tracker.dart
+++ bin/dart_expense_tracker.dart
@@ -11,7 +11,7 @@ void main() {
   printOptions();
   String? optionChosen;
 
-  while (optionChosen?.trim() != "4") {
+  while (true) {
     optionChosen = stdin.readLineSync()?.trim();
 
     switch (optionChosen) {
@@ -20,8 +20,12 @@ void main() {
       case "2":
         viewExpenses();
       case "3":
-        deleteExpense("1");
+        deleteExpense();
       case "4":
+        updateExpense();
+      case "5":
+        saveExpenses();
+      case "6":
         print("Bye bye!");
         return;
       default:
@@ -30,6 +34,79 @@ void main() {
 
     printOptions();
   }
+}
+
+void updateExpense() {
+  String? idInput;
+  print("Enter ID to update: ");
+  idInput = stdin.readLineSync()?.trim();
+
+  if (idInput == null) {
+    print("Invalid ID!");
+    return;
+  }
+
+  if (idInput.isEmpty) {
+    print("ID must be entered!");
+    return;
+  }
+
+  int? id = int.tryParse(idInput);
+
+  if (id == null) {
+    print("Invalid ID!");
+    return;
+  }
+
+  if (!expenses.containsKey(id)) {
+    print("ID does not exist!");
+    return;
+  }
+
+  Expense expense = expenses[id]!;
+
+  print("Update fields. Press enter to keep the field same\n");
+  print("Update description [${expense.description}]: ");
+  String? updatedDescription;
+  updatedDescription = stdin.readLineSync()?.trim();
+
+  if (updatedDescription == null || updatedDescription.isEmpty) {
+    updatedDescription = expense.description;
+  }
+
+  print("Update amount [${expense.amount}]: ");
+  String? updatedAmountInput;
+  updatedAmountInput = stdin.readLineSync()?.trim();
+
+  if (updatedAmountInput == null || updatedAmountInput.isEmpty) {
+    updatedAmountInput = expense.amount.toString();
+  }
+
+  double? updatedAmount = double.tryParse(updatedAmountInput);
+
+  if (updatedAmount == null) {
+    print("Invalid amount!");
+    return;
+  }
+
+  print("Update due date: ");
+  String? updatedDueDate;
+  updatedDueDate = stdin.readLineSync()?.trim();
+
+  if (updatedDueDate == null || updatedDueDate.isEmpty) {
+    updatedDueDate = expense.dueDate;
+  }
+
+  expense.amount = updatedAmount;
+  expense.description = updatedDescription;
+  expense.dueDate = updatedDueDate;
+
+  print("Expense updated!");
+
+}
+
+void saveExpenses() {
+  print("Expenses saved!");
 }
 
 void addExpense() {
@@ -73,33 +150,69 @@ void addExpense() {
   dueDateInput = stdin.readLineSync()?.trim();
 
   if (dueDateInput == null) {
-    print("Invalid description!");
+    print("Invalid due date!");
     return;
   }
 
   if (dueDateInput.isEmpty) {
-    print("Description cannot be empty!");
+    print("Due date cannot be empty!");
     return;
   }
 
   if (expenses.isNotEmpty) {
     Expense lastExpense = expenses.values.last;
-    Expense newExpense = Expense(lastExpense.getNextId, expenseAmount, descriptionInput, dueDateInput);
-    expenses[lastExpense.getNextId] = newExpense;
+    Expense newExpense = Expense(lastExpense.nextId, expenseAmount, descriptionInput, dueDateInput);
+    expenses[lastExpense.nextId] = newExpense;
   } else {
     Expense newExpense = Expense(0, expenseAmount, descriptionInput, dueDateInput);
     expenses[0] = newExpense;
   }
 
+  print("Expense added!");
+
 }
 
-void deleteExpense(String id) {
+void deleteExpense() {
+  String? idInput;
+  print("Enter ID to remove: ");
+  idInput = stdin.readLineSync()?.trim();
+
+  if (idInput == null) {
+    print("Invalid ID!");
+    return;
+  }
+
+  if (idInput.isEmpty) {
+    print("ID must be entered!");
+    return;
+  }
+
+  int? id = int.tryParse(idInput);
+
+  if (id == null) {
+    print("Invalid ID!");
+    return;
+  }
+
+  if (!expenses.containsKey(id)) {
+    print("ID does not exist!");
+    return;
+  }
+
+  expenses.remove(id);
+  print("Expense removed!");
 
 }
 
 void viewExpenses() {
+  if (expenses.isEmpty) {
+    print("\n-- NO EXPENSES --\n");
+    return;
+  }
   for (final expense in expenses.values) {
+    print("----------\n");
     print(expense);
+    print("----------\n");
   }
 }
 
@@ -109,9 +222,10 @@ void printOptions() {
   Welcome to expense tracker! Here are your options:
   1. Add Expense
   2. View Expenses
-  3. Delete Expenses
-  4. Save Expenses
-  5. Exit
+  3. Delete Expense
+  4. Update Expense
+  5. Save Expenses
+  6. Exit
   """;
   print(options);
 }
--- lib/expense.dart
+++ lib/expense.dart
@@ -1,36 +1,40 @@
 class Expense {
-  int id;
-  double amount;
-  String description;
-  String dueDate;
+  int _id;
+  double _amount;
+  String _description;
+  String _dueDate;
 
-  Expense(this.id, this.amount, this.description, this.dueDate);
+  Expense(int id, double amount, String description, String dueDate)
+    : this._id = id,
+      this._amount = amount,
+      this._description = description,
+      this._dueDate = dueDate;
 
-  double get getAmount => this.amount;
-  String get getDescription => this.description;
-  String get getDueDate => this.dueDate;
-  int get getId => this.id;
-  int get getNextId => this.id + 1;
+  double get amount => this._amount;
+  String get description => this._description;
+  String get dueDate => this._dueDate;
+  int get id => this._id;
+  int get nextId => this._id + 1;
 
-  set setAmount(double amount) {
-    this.amount = amount;
+  set amount(double amount) {
+    this._amount = amount;
   }
 
-  set setDescription(String description) {
-    this.description = description;
+  set description(String description) {
+    this._description = description;
   }
 
-  set setDueDate(String dueDate) {
-    this.dueDate = dueDate;
+  set dueDate(String dueDate) {
+    this._dueDate = dueDate;
   }
 
   @override
   String toString() {
     return """
-      EXPENSE ID: ${this.id}
-      AMOUNT: ${this.amount}
-      DESCRIPTION: ${this.description}
-      DUE DATE: ${this.dueDate}
+      EXPENSE ID: ${this._id}
+      AMOUNT: ${this._amount}
+      DESCRIPTION: ${this._description}
+      DUE DATE: ${this._dueDate}\n
     """;
 
   }
